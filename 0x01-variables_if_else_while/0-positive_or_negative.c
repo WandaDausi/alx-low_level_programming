840bc6049d83d1e027c1746a90b6b29916ea74d8
Tawanda Lincoln Dausi <tawiedausi@gmail.com>
2023-02-17 12:35:41 -0800

    0 positive negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+int main ()
+{
+int n;
+if (n>0)
+{
+printf("d% is a positive number\n,n");
+}
+if (n==0)
+{
+printf("%d is zero\n,n");
+}
+if (n<0)
+{
+printf("%d is a negative number\n,n");
+}
+return (0)
+}
