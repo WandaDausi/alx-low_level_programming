120bea0f9460b49095660e1bb44f818513e7ed3d
Tawanda Lincoln Dausi <tawiedausi@gmail.com>
2023-03-09 01:09:37 +0000

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * _puts_recursion - print a string 
- * Returns void
- * WandaStand
+ * _puts_recursion - print a string
+ * @s:string
+ * Return:void
  */
 void _puts_recursion(char *s)
 {
-	if(*s)
+if (*s)
 {
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
-	else
+else
 {
 	_putchar('\n');
 }
