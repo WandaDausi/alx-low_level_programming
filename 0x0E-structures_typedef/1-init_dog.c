f380e6b84696891ef184ec902284d748bfff887d
Tawanda Lincoln Dausi <tawiedausi@gmail.com>
2023-03-20 22:50:11 +0000

    tasks Structure , typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,11 +9,12 @@
  *
  * Return: void
  */
-void init_dog(struct dog *d, char *name, float age, char *owner)i
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
