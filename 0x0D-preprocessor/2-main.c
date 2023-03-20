a72cfeb2e5bd534e571447b1d072f08293229386
Tawanda Lincoln Dausi <tawiedausi@gmail.com>
2023-03-20 23:05:09 +0000

    task2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
-/*
- * a program that prints the name of the file it was compiled from
- * Return: 0
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
