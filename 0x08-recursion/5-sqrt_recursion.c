39643f76141bb72d1b5b8ba07b2aa486ef0095c0
Tawanda Dausi <tawiedausi@gmail.com>
2023-06-08 01:06:14 +0000

    tasks 1 correc

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,18 +1,6 @@
 #include "main.h"
 
 /**
- *  _sqrt_recursion - function that return the natural square root of a number
- *  @n: number
- *  Return: square root number
- */
-int _sqrt_recursion(int n)
-{
-	if i(n == 0)
-		return (0);
-	return (sqr_recur(n, 1));
-}
-
-/**
  * sqr_recur - finds the natural square root
  * @a: number
  * @b: number
@@ -25,4 +13,16 @@ int sqr_recur(int a, int b)
 	if (a * a > b)
 		return (-1);
 	return (sqr_recur(a + 1, b));
+}
+
+/**
+ *  _sqrt_recursion - function that return the natural square root of a number
+ *  @n: number
+ *  Return: square root number
+ */
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (0);
+	return (sqr_recur(n, 1));
 }
