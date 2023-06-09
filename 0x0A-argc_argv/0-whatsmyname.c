c36f98837bc316ac9e6cde314b4f9b5091c596be
Tawanda Dausi <tawiedausi@gmail.com>
2023-06-09 14:26:46 +0000

    correc task0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,12 @@
 /**
  * main - prints its name
  * @argc: counts
- * @*argv: pointer to string of pointers
+ * @argv: pointer to string of pointers
  * Return: 0
  */
 int main(int argc, char *argv[])
 {
-	while (argv[argc])
+	while (argc--)
 		printf("%s\n", argv[0]);
 	return (0);
 }
