eebbada1661d0cbe1f567623766084feecb8fa20
Tawanda Dausi <tawiedausi@gmail.com>
2023-06-15 00:33:39 +0000

    four

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * **strtow - splits a string into words
+ * @str - pointer to string to be manipulated
+ * Return: NULL. pointer
+ */
+char **strtow(char *str)
+{
+	int i, j. l;
+	char ** words;
+
+	if (str == NULL || str[0] == '\0' || (str[0] == '' && str[1] == '\0'))
+		return (NULL);
+
+
+
+}
