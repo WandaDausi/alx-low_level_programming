3496e76489c973ba6d80964c99435b549e43ff3f
Tawanda Dausi <tawiedausi@gmail.com>
2023-06-17 02:24:48 +0000

    three

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -17,10 +17,11 @@ int *array_range(int min, int max)
 
 	size = max - min + 1;
 
-	arr = malloc(sizeof(arr) * size);
+	arr = malloc(sizeof(int) * size);
 
 	if (arr == NULL)
 		return (NULL);
+
 	for (j = min; j <= max; j++)
 		arr[j] = min++;
 
