c53eacdec730312da1bd49a6b6bd0cc9caf67c66
Tawanda Dausi <tawiedausi@gmail.com>
2023-07-07 01:57:46 +0200

    set value to 1

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1
+ * @n: pointer to decimal
+ * @index: index
+ * Return: 1 or -1
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i;
+
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+	i = (1 << index);
+	*n = *n | i;
+	return (1);
+
+}
