b9e8faebe36c7a1c65a63f32bc9d5e1127c50411
Tawanda Dausi <tawiedausi@gmail.com>
2023-07-07 02:47:01 +0200

    task6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * @void: parameter
+ * Return: 0 or 1
+ */
+int get_endianness(void)
+{
+	int i;
+	char *c;
+
+	i = 1;
+	c = (char *)&i;
+
+	return (*c);
+}
