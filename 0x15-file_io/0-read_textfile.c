b5c8a48de1be89bc6192a8aade69409fabcb2f9b
Tawanda Dausi <tawiedausi@gmail.com>
2023-07-11 02:55:54 +0200

    0zero

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX standard output
+ * @filename: name of file to be printed
+ * @letters: letters
+ * Return: NULL or number or actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *acts;
+
+	if (filename == NULL)
+		return (0);
+
+	acts = malloc(sizeof(char) * letters);
+	if (acts == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, acts, letters);
+	w = write(STDOUT_FILENO, acts, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(acts);
+		return (0);
+	}
+	free(acts);
+	close(0);
+
+	return (w);
+}
