376d4195d42b8120adb72f916965bcdd5a44a3cc
Tawanda Dausi <tawiedausi@gmail.com>
2023-08-08 02:53:13 +0000

    ii

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,21 +8,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, leng;
+	int o, w, leng = 0;
+
+	if (filename == NULL)
+		return (-1);
 
 	if (text_content != NULL)
 	{
-		leng = 0;
-		while (text_content[leng])
+		for (leng = 0; text_content[leng];)
 			leng++;
 	}
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(o, text_content, leng);
 
 	if (o == -1 || w == -1)
-		return (-1);
-
-	if (filename == NULL)
 		return (-1);
 
 	close(o);
